2d38a6e1a5846c94ba4ded7ed0ee9482862f4577
tim-hogan <tim@mobilelocate.co.nz>
2021-01-17 10:28:42 +1300

    Create sig.js

---
+++ js/sig.js
@@ -0,0 +1,94 @@
+﻿var b64map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var b64pad = "=";
+var BI_RM = "0123456789abcdefghijklmnopqrstuvwxyz";
+function hex2b64(d) {
+    var b;
+    var e;
+    var a = "";
+    for (b = 0; b + 3 <= d.length; b += 3) {
+        e = parseInt(d.substring(b, b + 3), 16);
+        a += b64map.charAt(e >> 6) + b64map.charAt(e & 63)
+    }
+    if (b + 1 == d.length) {
+        e = parseInt(d.substring(b, b + 1), 16);
+        a += b64map.charAt(e << 2)
+    } 
+    else {
+        if (b + 2 == d.length) {
+            e = parseInt(d.substring(b, b + 2), 16);
+            a += b64map.charAt(e >> 2) + b64map.charAt((e & 3) << 4)
+        }
+    }
+    if (b64pad) {
+        while ((a.length & 3) > 0) {
+            a += b64pad
+        }
+    }
+    return a
+}
+
+function b64tohex(f) {
+    var d = "";
+    var e;
+    var b = 0;
+    var c;
+    var a;
+    for (e = 0; e < f.length; ++e) {
+        if (f.charAt(e) == b64pad) {
+            break
+        }
+        a = b64map.indexOf(f.charAt(e));
+        if (a < 0) {
+            continue
+        }
+        if (b == 0) {
+            d += int2char(a >> 2);
+            c = a & 3;
+            b = 1
+        }
+        else {
+            if (b == 1) {
+                d += int2char((c << 2) | (a >> 4));
+                c = a & 15;
+                b = 2
+            }
+            else {
+                if (b == 2) {
+                    d += int2char(c);
+                    d += int2char(a >> 2);
+                    c = a & 3;
+                    b = 3
+                }
+                else {
+                    d += int2char((c << 2) | (a >> 4));
+                    d += int2char(a & 15);
+                    b = 0
+                }
+            }
+        }
+    }
+    if (b == 1) {
+        d += int2char(c << 2)
+    }
+    return d
+}
+
+function int2char(a) {
+    return BI_RM.charAt(a)
+}
+
+function BigInteger(e, d, f) {
+    if (e != null) {
+        if ("number" == typeof e) {
+            this.fromNumber(e, d, f)
+        }
+        else {
+            if (d == null && "string" != typeof e) {
+                this.fromString(e, 256)
+            }
+            else {
+                this.fromString(e, d)
+            }
+        }
+    } 
+}
