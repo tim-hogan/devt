26f4e2a252b20ad072da2af3d8b24990b0eb08de
tim-hogan <tim@mobilelocate.co.nz>
2021-01-29 06:54:28 +1300

    Update FormBuilder.php

--- FormBuilder.php
+++ FormBuilder.php
@@ -19,12 +19,28 @@ $DB = new nvaluatecorp($devt_environment->getDatabaseParameters());
 require "./includes/classFormList2.php";
 
 $r = $DB->query("show tables");
-$a = $r->fetch_all(MYSQLI_ASSOC);
+$a = $r->fetch_all(MYSQLI_NUM);
 foreach($a as $table)
 {
     echo "<p>";
     var_dump($table);
     echo "</p>";
+
+    $name = $table[0];
+
+    echo "<p>Table name: ";
+    var_dump($name);
+    echo "</p>";
+
+    $finfo = $DB->fieldsFromTable($table);
+
+    foreach ($finfo as $field)
+    {
+        echo "<p>   Field Info: ";
+        var_dump($field);
+        echo "</p>";
+    }
+
 }
 
 
