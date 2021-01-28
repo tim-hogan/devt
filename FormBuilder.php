24dbeb04e3916ea1f7f898847ceba74703562c04
tim-hogan <tim@mobilelocate.co.nz>
2021-01-29 07:13:19 +1300

    Update FormBuilder.php

--- FormBuilder.php
+++ FormBuilder.php
@@ -1,6 +1,27 @@
 <?php
 session_start();
 
+/*
+ * Define the field flags
+ *
+*/
+define('FIELD_NOT_NULL_FLAG',1);
+define('FIELD_PRI_KEY_FLAG', 2);
+define('FIELD_UNIQUE_KEY_FLAG', 4);
+define('FIELD_BLOB_FLAG', 16);
+define('FIELD_UNSIGNED_FLAG', 32);
+define('FIELD_ZEROFILL_FLAG', 64);
+define('FIELD_BINARY_FLAG', 128);
+define('FIELD_ENUM_FLAG', 256);
+define('FIELD_AUTO_INCREMENT_FLAG', 512);
+define('FIELD_TIMESTAMP_FLAG', 1024);
+define('FIELD_SET_FLAG', 2048);
+define('FIELD_NUM_FLAG', 32768);
+define('FIELD_PART_KEY_FLAG', 16384);
+define('FIELD_GROUP_FLAG', 32768);
+define('FIELD_UNIQUE_FLAG', 65536);
+
+
 function var_error_log( $object=null,$text='')
 {
     ob_start();
@@ -18,26 +39,40 @@ $DB = new nvaluatecorp($devt_environment->getDatabaseParameters());
 
 require "./includes/classFormList2.php";
 
-$r = $DB->query("show tables");
-$a = $r->fetch_all(MYSQLI_NUM);
-foreach($a as $table)
-{
-    $name = $table[0];
-
-    echo "<p>Table name: ";
-    var_dump($name);
-    echo "</p>";
-
-    $finfo = $DB->fieldsFromTable($table[0]);
-
-    foreach ($finfo as $field)
-    {
-        echo "<p>   Field Info: ";
-        var_dump($field);
-        echo "</p>";
-    }
-
-}
-
-
 ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="initial-scale=1.0" />
+    <title>FromBuilder</title>
+</head>
+<body>
+    <?php
+    $r = $DB->query("show tables");
+    $a = $r->fetch_all(MYSQLI_NUM);
+    foreach($a as $table)
+    {
+        $name = $table[0];
+
+        echo "<p>Table name: ";
+        var_dump($name);
+        echo "</p>";
+
+        $finfo = $DB->fieldsFromTable($table[0]);
+
+        foreach ($finfo as $field)
+        {
+            echo "<p> Field Info: ";
+            var_dump($field);
+
+            if ($field->flags & FIELD_PRI_KEY_FLAG)
+            {
+                echo " PRIMARY KEY";
+            }
+            echo "</p>";
+        }
+    }
+    ?>
+</body>
+</html>
