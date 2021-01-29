19e215dcad8390446bd7c354890b0b4fa27c5f09
tim-hogan <tim@mobilelocate.co.nz>
2021-01-29 20:29:38 +1300

    Update FormBuilder.php

--- FormBuilder.php
+++ FormBuilder.php
@@ -116,217 +116,238 @@ require "./includes/classFormList2.php";
 
 
 <?php
-$tabledefs = array();
-$r = $DB->query("show tables");
-$a = $r->fetch_all(MYSQLI_NUM);
-foreach($a as $table)
+function buildDefault()
 {
-
-
-    $finfo = $DB->fieldsFromTable($table[0]);
-
-    $params = array();
-    $global = array();
-    $form = array();
-
-    $global['table'] = $table[0];
-    $form['heading'] = "{$table[0]} Heading";
-    $form['introduction'] = "";
-
-    $classes = array();
-
-    $div = array();
-
-    $div["inputtext"] = "d_inputtext";
-    $div["emailtext"] = "d_inputtext";
-    $div["passwordtext"] = "d_inputtext";
-    $div["textarea"] = "d_textarea";
-    $div["checkbox"] = "d_inputtext";
-    $div["choice"] = "d_choice";
-    $div["dropdown"] = "d_dropdown";
-    $div["fk"] = "d_dropdown";
-
-
-    $classes['div'] = $div;
-    $form['classes'] = $classes;
-
-    $groups = array();
-    $deatils1 = array();
-    $deatils1['heading'] = "GROUP NAME";
-    $deatils1['introduction1'] = "";
-    $deatils1['introduction2'] = "";
-    $deatils1['introduction3'] = "";
-
-    $groups['details1'] = $deatils1;
-    $form['groups'] = $groups;
-
-
-    $list = array();
-
-    $list['type'] = "checkbox";
-    $list['record_selector'] = true;
-    $list['heading'] = "{$table[0]} List heading text";
-    $list['introduction'] = "";
-    $list['default_order'] = "";
-    $list['default_where'] = "where {$table[0]}_deleted = 0";
-
-    $fields = array();
-
-
-    $doneanchor = false;
-    foreach ($finfo as $field)
+    $tabledefs = array();
+    $r = $DB->query("show tables");
+    $a = $r->fetch_all(MYSQLI_NUM);
+    foreach($a as $table)
     {
-        if ($field->flags & FIELD_AUTO_INCREMENT_FLAG)
+
+
+        $finfo = $DB->fieldsFromTable($table[0]);
+
+        $params = array();
+        $global = array();
+        $form = array();
+
+        $global['table'] = $table[0];
+        $form['heading'] = "{$table[0]} Heading";
+        $form['introduction'] = "";
+
+        $classes = array();
+
+        $div = array();
+
+        $div["inputtext"] = "d_inputtext";
+        $div["emailtext"] = "d_inputtext";
+        $div["passwordtext"] = "d_inputtext";
+        $div["textarea"] = "d_textarea";
+        $div["checkbox"] = "d_inputtext";
+        $div["choice"] = "d_choice";
+        $div["dropdown"] = "d_dropdown";
+        $div["fk"] = "d_dropdown";
+
+
+        $classes['div'] = $div;
+        $form['classes'] = $classes;
+
+        $groups = array();
+        $deatils1 = array();
+        $deatils1['heading'] = "GROUP NAME";
+        $deatils1['introduction1'] = "";
+        $deatils1['introduction2'] = "";
+        $deatils1['introduction3'] = "";
+
+        $groups['details1'] = $deatils1;
+        $form['groups'] = $groups;
+
+
+        $list = array();
+
+        $list['type'] = "checkbox";
+        $list['record_selector'] = true;
+        $list['heading'] = "{$table[0]} List heading text";
+        $list['introduction'] = "";
+        $list['default_order'] = "";
+        $list['default_where'] = "where {$table[0]}_deleted = 0";
+
+        $fields = array();
+
+
+        $doneanchor = false;
+        foreach ($finfo as $field)
         {
-            $global['primary_key'] = $field->name;
+            if ($field->flags & FIELD_AUTO_INCREMENT_FLAG)
+            {
+                $global['primary_key'] = $field->name;
+            }
+
+            $fdata = array();
+            $fdata["dbfield"] = true;
+            $fdata["fk_table"] = "";
+            $fdata["fk_index"] = "";
+            $fdata["fk_display"] = "";
+            $fdata["fk_where"] = "";
+            $fdata["fk_order"] = "";
+
+            $fdata["maxlength"] = $field->max_length;
+            $fdata["cols"] = "50";
+            $fdata["rows"] = "4";
+            $fdata["errname"] = $field->name;
+
+            $ff = array();
+            $ff["display"] = true;
+            $ff["formlabel"] = $field->name;
+            $ff["title"] = "";
+            if ($field->flags & FIELD_NOT_NULL_FLAG)
+                $ff["required"] = true;
+            else
+                $ff["required"] = false;
+
+            $ff["default"] = "";
+            $ff["errtext"] = "";
+            $ff["posttext"] = "";
+            $ff["trim"] =  true;
+            $ff["group"] = "details1";
+
+            $choice = array();
+
+            $choiceEntry1 = array();
+            $choiceEntry1["text"] = "Text 1";
+            $choiceEntry1["value"] = "1";
+            $choiceEntry1["selected"] = "javascript()";
+
+            array_push($choice,$choiceEntry1);
+
+            $choiceEntry2 = array();
+            $choiceEntry2["text"] = "Text 2";
+            $choiceEntry2["value"] = "2";
+            $choiceEntry2["selected"] = "javascript()";
+
+            array_push($choice,$choiceEntry2);
+
+            $ff["choice"] = $choice;
+
+
+            $fdata["form"] = $ff;
+
+
+            $fl = array();
+            $fl["display"] = true;
+            $fl["heading"] = $field->name;
+
+            if (! $doneanchor && !($field->flags & FIELD_PRI_KEY_FLAG) )
+            {
+                $fl["anchor"] = true;
+                $doneanchor = true;
+            }
+            else
+                $fl["anchor"] = false;
+            $fl["displayoption"] = "";
+
+            $ff["list"] = $fl;
+
+            switch ($field->type)
+            {
+                case FIELD_TYPE_TINYINT:
+                    $fdata['type'] = 'boolean';
+                    $fdata['tag'] = "checkbox";
+                    $fdata['sub-tag'] = "";
+                    break;
+
+                case FIELD_TYPE_SMALLINT:
+                case FIELD_TYPE_INTEGER:
+                case FIELD_TYPE_BIGINT:
+                    $fdata['type'] = 'integer';
+                    $fdata['tag'] = "input";
+                    $fdata['sub-tag'] = "text";
+                    break;
+
+                case FIELD_TYPE_FLOAT:
+                case FIELD_TYPE_DOUBLE:
+                case FIELD_TYPE_DECIMAL:
+                    $fdata['type'] = 'decimal';
+                    $fdata['tag'] = "input";
+                    $fdata['sub-tag'] = "text";
+                    break;
+
+                case FIELD_TYPE_TIMESTAMP:
+                case FIELD_TYPE_DATETIME:
+                    $fdata['type'] = 'datetime';
+                    $fdata['tag'] = "input";
+                    $fdata['sub-tag'] = "datetime-local";
+                    break;
+
+                case FIELD_TYPE_DATE:
+                    $fdata['type'] = 'date';
+                    $fdata['tag'] = "input";
+                    $fdata['sub-tag'] = "date";
+                    break;
+
+                case FIELD_TYPE_TIME:
+                    $fdata['type'] = 'time';
+                    $fdata['tag'] = "input";
+                    $fdata['sub-tag'] = "time";
+                    break;
+
+                case FIELD_TYPE_YEAR:
+                    $fdata['type'] = 'year';
+                    $fdata['tag'] = "input";
+                    $fdata['sub-tag'] = "text";
+                    break;
+                case FIELD_TYPE_DECIMAL:
+                case FIELD_TYPE_VARCHAR:
+                case FIELD_TYPE_CHAR:
+                    $fdata['type'] = 'text';
+                    $fdata['tag'] = "input";
+                    $fdata['sub-tag'] = "text";
+                    break;
+
+                default:
+                    break;
+            }
+
+            $fields[$field->name] = $fdata;
+
         }
 
-        $fdata = array();
-        $fdata["dbfield"] = true;
-        $fdata["fk_table"] = "";
-        $fdata["fk_index"] = "";
-        $fdata["fk_display"] = "";
-        $fdata["fk_where"] = "";
-        $fdata["fk_order"] = "";
-
-        $fdata["maxlength"] = $field->max_length;
-        $fdata["cols"] = "50";
-        $fdata["rows"] = "4";
-        $fdata["errname"] = $field->name;
-
-        $ff = array();
-        $ff["display"] = true;
-        $ff["formlabel"] = $field->name;
-        $ff["title"] = "";
-        if ($field->flags & FIELD_NOT_NULL_FLAG)
-            $ff["required"] = true;
-        else
-            $ff["required"] = false;
-
-        $ff["default"] = "";
-        $ff["errtext"] = "";
-        $ff["posttext"] = "";
-        $ff["trim"] =  true;
-        $ff["group"] = "details1";
-
-        $choice = array();
-
-        $choiceEntry1 = array();
-        $choiceEntry1["text"] = "Text 1";
-        $choiceEntry1["value"] = "1";
-        $choiceEntry1["selected"] = "javascript()";
-
-        array_push($choice,$choiceEntry1);
-
-        $choiceEntry2 = array();
-        $choiceEntry2["text"] = "Text 2";
-        $choiceEntry2["value"] = "2";
-        $choiceEntry2["selected"] = "javascript()";
-
-        array_push($choice,$choiceEntry2);
-
-        $ff["choice"] = $choice;
+        $params['global'] = $global;
+        $params['form'] = $form;
+        $params['list'] = $list;
+        $params['fields'] = $fields;
 
 
-        $fdata["form"] = $ff;
-
-
-        $fl = array();
-        $fl["display"] = true;
-        $fl["heading"] = $field->name;
-
-        if (! $doneanchor && !($field->flags & FIELD_PRI_KEY_FLAG) )
-        {
-            $fl["anchor"] = true;
-            $doneanchor = true;
-        }
-        else
-            $fl["anchor"] = false;
-        $fl["displayoption"] = "";
-
-        $ff["list"] = $fl;
-
-        switch ($field->type)
-        {
-            case FIELD_TYPE_TINYINT:
-                $fdata['type'] = 'boolean';
-                $fdata['tag'] = "checkbox";
-                $fdata['sub-tag'] = "";
-                break;
-
-            case FIELD_TYPE_SMALLINT:
-            case FIELD_TYPE_INTEGER:
-            case FIELD_TYPE_BIGINT:
-                $fdata['type'] = 'integer';
-                $fdata['tag'] = "input";
-                $fdata['sub-tag'] = "text";
-                break;
-
-            case FIELD_TYPE_FLOAT:
-            case FIELD_TYPE_DOUBLE:
-            case FIELD_TYPE_DECIMAL:
-                $fdata['type'] = 'decimal';
-                $fdata['tag'] = "input";
-                $fdata['sub-tag'] = "text";
-                break;
-
-            case FIELD_TYPE_TIMESTAMP:
-            case FIELD_TYPE_DATETIME:
-                $fdata['type'] = 'datetime';
-                $fdata['tag'] = "input";
-                $fdata['sub-tag'] = "datetime-local";
-                break;
-
-            case FIELD_TYPE_DATE:
-                $fdata['type'] = 'date';
-                $fdata['tag'] = "input";
-                $fdata['sub-tag'] = "date";
-                break;
-
-            case FIELD_TYPE_TIME:
-                $fdata['type'] = 'time';
-                $fdata['tag'] = "input";
-                $fdata['sub-tag'] = "time";
-                break;
-
-            case FIELD_TYPE_YEAR:
-                $fdata['type'] = 'year';
-                $fdata['tag'] = "input";
-                $fdata['sub-tag'] = "text";
-                break;
-            case FIELD_TYPE_DECIMAL:
-            case FIELD_TYPE_VARCHAR:
-            case FIELD_TYPE_CHAR:
-                $fdata['type'] = 'text';
-                $fdata['tag'] = "input";
-                $fdata['sub-tag'] = "text";
-                break;
-
-            default:
-                break;
-        }
-
-        $fields[$field->name] = $fdata;
+        $tabledefs[$table[0]] = $params;
 
     }
-
-    $params['global'] = $global;
-    $params['form'] = $form;
-    $params['list'] = $list;
-    $params['fields'] = $fields;
-
-
-    $tabledefs[$table[0]] = $params;
-
+    return $tabledefs;
 }
 
-//Output text
-$strtext = '';
+function OutputToFile($t)
+{
+    $strtext = '';
+    $strtext = outputArray($t,0);
+    file_put_contents("/var/nvaluate/formbuilder/formparams.php",$strtext);
+}
 
-$strtext = outputArray($tabledefs,0);
+$g_def = null;
+if (isset($_SESSION['def']))
+    $g_def = $_SESSION['def'];
 
-file_put_contents("/var/nvaluate/formbuilder/formparams.php",$strtext);
-
+if (isset($_GET['v']))
+{
+    if ($_GET['v'] == 'buildfromdb')
+    {
+        $g_def = buildDefault();
+        $_SESSION['def'] = $g_def;
+    }
+    if ($_GET['v'] == 'output')
+    {
+        if ($g_def)
+            OutputToFile($g_def);
+    }
+}
 ?>
 
 <!DOCTYPE HTML>
@@ -348,18 +369,19 @@ file_put_contents("/var/nvaluate/formbuilder/formparams.php",$strtext);
 
         </div>
         <div id="menu">
-
+            <div><a href="FormBuilder.php?v=buildfromdb">BUILD FROM DATABASE</a></div>
+            <div><a href="FormBuilder.php?v=output">SAVE TOP FILE</a></div>
         </div>
         <div id="main">
             <div id="flex">
                 <div id="left">
                     <ul>
-                    <?php
-                    foreach ($tabledefs as $name => $table)
-                    {
-                        echo "<li>{$name}</li>";    
-                    }
-                    ?>
+                        <?php
+                        foreach ($g_def as $name => $table)
+                        {
+                            echo "<li><a href='FormBuilder.php?t={$name}'>{$name}</a></li>";
+                        }
+                        ?>
                     </ul>
                 </div>
                 <div id="right1">
