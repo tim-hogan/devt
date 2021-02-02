dc5f8de82dd30f7f3ad10341227ba03db1f7715c
tim-hogan <tim@mobilelocate.co.nz>
2021-02-03 05:57:32 +1300

    Update FormBuilder.php

--- FormBuilder.php
+++ FormBuilder.php
@@ -497,6 +497,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
         }
     }
+
+    if (isset($_POST["loadform"])
+    {
+        if (isset(($_POST["filename"]))
+        {
+            $filename = trim($_POST["filename"]);
+            if (file_exists($filename))
+                $g_def = include ($filename);
+        }
+
+    }
+
     $_SESSION['def'] = $g_def;
 }
 
