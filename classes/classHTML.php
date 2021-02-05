09a19d0fea11e9a9158b8001d8f7705dc9124663
tim-hogan <tim@mobilelocate.co.nz>
2021-02-05 13:47:37 +1300

    Create classHTML.php

---
+++ classes/classHTML.php
@@ -0,0 +1,49 @@
+<?php
+namespace devt\HTML;
+class htmlElement
+{
+    private $_tag;
+    private $_attributes = array();
+    private $_children = array();
+
+    function __construct($tag)
+    {
+        $this->_tag = $tag;
+    }
+
+    function insertChild($child)
+    {
+        array_push($this->_children,$child);
+    }
+
+    function addAttribute($name,$value)
+    {
+        array_push($this->_attributes,[$name => $value]);
+    }
+
+    function toString()
+    {
+        $ret = "<{$this->_tag} ";
+        foreach ($this->_attributes as $name => $value)
+        {
+            $ret .= "{$name}='{$value}'";
+        }
+        $ret .= ">";
+        foreach ($this->_children as $child)
+        {
+            $ret .=  $child->toString();
+        }
+        $ret .= "</{$this->_tag}>";
+        return $ret;
+    }
+}
+
+class htmlForm extends htmlElement
+{
+    function __construct($method="POST")
+    {
+        parent::__construct("form");
+        $this->addAttribute("method",$method);
+    }
+}
+?>
