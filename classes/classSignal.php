5251703c61f9d8b0d1933bcb999859c1e8b25a2d
tim-hogan <tim@mobilelocate.co.nz>
2021-06-20 16:48:49 +1200

    Update classSignal.php

--- classes/classSignal.php
+++ classes/classSignal.php
@@ -3,6 +3,7 @@ class Signal
 {
     private $_socksend = null;
     private $_sockrecv = null;
+    private $_queue = null;
     private $_port;
     private $_ip = "255.255.255.255";
 
@@ -40,6 +41,27 @@ class Signal
             socket_set_option($this->_socksend, SOL_SOCKET, SO_BROADCAST, 1);
         }
         socket_sendto($this->_socksend, $data, strlen($data), 0, $this->_ip, $this->_port);
+    }
+
+    public function queue($data)
+    {
+        if (! $this->_queue)
+            $this->_queue = msg_get_queue($this->_port);
+        msg_send($this->_queue, 1, $data);
+    }
+
+    public function receive()
+    {
+        $data = null;
+        if (! $this->_queue)
+            $this->_queue = msg_get_queue($this->_port);
+        $stat = msg_stat_queue($this->_queue);
+        if ($stat['msg_qnum'] > 0)
+        {
+            msg_receive($this->_queue, 1, $msgtype, 1024, $data);
+            return $data;
+        }
+        return null;
     }
 }
 ?>
