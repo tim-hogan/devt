f839cccaaa53176259b9bb82d5fc9c2187782f08
tim-hogan <tim@mobilelocate.co.nz>
2023-11-03 11:44:24 +1300

    Update

--- classes/classCollectionCSV.php
+++ classes/classCollectionCSV.php
@@ -24,7 +24,7 @@ class CSVCollection extends CCollection
 				if (empty($this->_keys))
 				{
 					$this->_keys = $row;
-					$this->_keys[0] = $this->removeUnicodeString($this->_keys[0]);
+					$this->_keys[0] = self::removeUnicodeString($this->_keys[0]);
 					continue;
 				}
 
@@ -59,7 +59,7 @@ class CSVCollection extends CCollection
 	// Private functions
 	//****************************************************************************************************
 
-	private function removeUnicodeString($s)
+	static public function removeUnicodeString($s)
 	{
 		if (substr(bin2hex($s), 0, 6) == "efbbbf")
 			return substr($s, 3);
@@ -67,5 +67,159 @@ class CSVCollection extends CCollection
 			return $s;
 	}
 
+	static public function fileColumnKeys($filename)
+	{
+		$keys = array();
+
+		if (!file_exists($filename))
+			throw new Exception('File {$filename} does not exist');
+
+		$f = fopen($filename, "r");
+		if (!$f)
+				throw new Exception('Unable to open {$filename} in read mode');
+
+		while (($row = fgetcsv($f, 32000)) !== false)
+		{
+			if (empty($keys))
+			{
+				$keys = $row;
+				$keys[0] = self::removeUnicodeString($keys[0]);
+				break;
+			}
+		}
+		return $keys;
+	}
+
+	static public function enumerateAll($filename, $key)
+	{
+		$ret = array();
+		$keys = array();
+
+		if (!file_exists($filename))
+			throw new Exception('File {$filename} does not exist');
+
+		$f = fopen($filename, "r");
+		if (!$f)
+			throw new Exception('Unable to open {$filename} in read mode');
+
+		while (($row = fgetcsv($f, 32000)) !== false) {
+			if (empty($keys)) {
+				$keys = $row;
+				$keys[0] = self::removeUnicodeString($keys[0]);
+				continue;
+			}
+
+
+			if (array_search($key, $keys) === false)
+				return null;
+			$idx = 0;
+			foreach ($keys as $k)
+			{
+				if (isset($row[$idx]))
+					$d[$k] = $row[$idx];
+				else
+					$d[$k] = null;
+				$idx++;
+			}
+
+			if (isset($d[$key]) && array_search($d[$key], $ret) === false)
+				$ret[] = $d[$key];
+		}
+		return $ret;
+	}
+
+	static public function firstRow($filename, $key,$value)
+	{
+		$keys = array();
+
+		if (!file_exists($filename))
+			throw new Exception('File {$filename} does not exist');
+
+		$f = fopen($filename, "r");
+		if (!$f)
+			throw new Exception('Unable to open {$filename} in read mode');
+
+		while (($row = fgetcsv($f, 32000)) !== false)
+		{
+			if (empty($keys)) {
+				$keys = $row;
+				$keys[0] = self::removeUnicodeString($keys[0]);
+				continue;
+			}
+
+
+			if (array_search($key, $keys) === false)
+				return null;
+
+			$idx = 0;
+			foreach ($keys as $k) {
+				if (isset($row[$idx]))
+					$d[$k] = $row[$idx];
+				else
+					$d[$k] = null;
+				$idx++;
+			}
+
+			if (isset($d[$key]) && $d[$key] == $value)
+				return $d;
+		}
+		return null;
+
+	}
+
+	static public function stats($filename,$column,$subsetcolumn = null,$subsetvalue = null)
+	{
+		$ret = array();
+		$keys = array();
+		$haveStats = false;
+
+		if (!file_exists($filename))
+			throw new Exception('File {$filename} does not exist');
+
+		$f = fopen($filename, "r");
+		if (!$f)
+			throw new Exception('Unable to open {$filename} in read mode');
+
+		$ret["min"] = PHP_FLOAT_MAX;
+		$ret["max"] = PHP_FLOAT_MIN;
+		$ret["cnt"] = 0;
+		$ret["sum"] = 0;
+
+		while (($row = fgetcsv($f, 32000)) !== false)
+		{
+			if (empty($keys)) {
+				$keys = $row;
+				$keys[0] = self::removeUnicodeString($keys[0]);
+				continue;
+			}
+
+			if ($column && array_search($column, $keys) === false)
+				return null;
+
+			$idx = 0;
+			foreach ($keys as $k)
+			{
+				if (isset($row[$idx]))
+					$d[$k] = $row[$idx];
+				else
+					$d[$k] = null;
+				$idx++;
+			}
+
+			if (!$subsetcolumn || $d[$subsetcolumn] == $subsetvalue)
+			{
+				if ($ret["min"] > $d[$column])
+					$ret["min"] = $d[$column];
+				if ($ret["max"] < $d[$column])
+					$ret["max"] = $d[$column];
+				$ret["cnt"]++;
+				$ret["sum"] += $d[$column];
+				$haveStats = true;
+			}
+		}
+		if ($haveStats)
+			return $ret;
+		return null;
+	}
 }
 ?>
