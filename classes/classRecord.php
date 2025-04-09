176baeb1b5972713ed7c024cf4a9559b2e5eddfc
tim-hogan <tim@mobilelocate.co.nz>
2025-04-10 10:27:14 +1200

    Update

--- classes/classRecord.php
+++ classes/classRecord.php
@@ -1,79 +1,163 @@
 <?php
 class CRecord
 {
-    private $_fields = null;
-    public function __construct($fields)
-    {
-        //Fields look like
-        //["name" => "fred" , "type" => "unit16_t", "length" => 50], ...
+	private $_fields = null;
+	private $_data = null;
 
-        $this->_fields = $fields;
+	public function __construct($fields)
+	{
+		//Fields look like
+		//["name" => "fred" , "type" => "unit16_t", "length" => 50], ...
+
+		$this->_fields = $fields;
+		$this->_data = array();
+	}
+
+	private function uint8_t(int $v)
+	{
+		if ($v > 255)
+			return false;
+		return hex2bin(str_pad(dechex($v), 2, "0", STR_PAD_LEFT));
+	}
+
+    private function uint16_t(int $v)
+	{
+		if ($v > 65536)
+			return false;
+		$t = hex2bin(str_pad(dechex($v), 4, "0", STR_PAD_LEFT));
+		return substr($t, 1, 1) . substr($t, 0, 1);
+	}
+
+    private function uint32_t(int $v)
+	{
+		if ($v > 4294967296)
+			return false;
+		$t = hex2bin(str_pad(dechex($v), 8, "0", STR_PAD_LEFT));
+		return substr($t, 3, 1) . substr($t, 2, 1) . substr($t, 1, 1) . substr($t, 0, 1);
+	}
+
+    private function uint64_t(int $v)
+	{
+		$t = hex2bin(str_pad(dechex($v), 16, "0", STR_PAD_LEFT));
+		return substr($t, 7, 1) . substr($t, 6, 1) . substr($t, 5, 1) . substr($t, 4, 1) . substr($t, 3, 1) . substr($t, 2, 1) . substr($t, 1, 1) . substr($t, 0, 1);
+	}
+
+    private function char(string $v, $l)
+	{
+		$sz = strlen($v);
+		if ($sz > $l - 1)
+			return false;
+		for ($i = 0; $i < $l - $sz; $i++)
+			$v .= hex2bin("00");
+		return $v;
+	}
+
+    private function strto_uint8_t(string $v)
+	{
+		return hexdec(bin2hex($v));
+	}
+
+    private function strto_uint16_t(string $v)
+	{
+		return hexdec(bin2hex(substr($v, 0, 1))) + (hexdec(bin2hex(substr($v, 1, 1))) * 256);
+	}
+
+    private function strto_uint32_t(string $v)
+    {
+        return hexdec(bin2hex(substr($v, 0, 1))) + (hexdec(bin2hex(substr($v, 1, 1))) * 256) + (hexdec(bin2hex(substr($v, 2, 1))) * 65536 ) + (hexdec(bin2hex(substr($v, 3, 1))) * 16777216);
     }
 
-    public function uint8_t(int $v)
+    private function strto_uint64_t(string $v)
     {
-        if ($v > 255)
-            return false;
-        return hex2bin(str_pad(dechex($v), 2, "0", STR_PAD_LEFT));
+        return
+		($this->strto_uint32_t(substr($v,4,4)) * 4294967296) +
+		 hexdec(bin2hex(substr($v, 0, 1))) + (hexdec(bin2hex(substr($v, 1, 1))) * 256) + (hexdec(bin2hex(substr($v, 2, 1))) * 65536) + (hexdec(bin2hex(substr($v, 3, 1))) * 16777216);
     }
 
-    public function uint16_t(int $v)
-    {
-        if ($v > 65536)
-            return false;
-        $t = hex2bin(str_pad(dechex($v), 4, "0", STR_PAD_LEFT));
-        return substr($t, 1, 1) . substr($t, 0, 1);
-    }
-
-    public function uint32_t(int $v)
-    {
-        if ($v > 4294967296)
-            return false;
-        $t = hex2bin(str_pad(dechex($v), 8, "0", STR_PAD_LEFT));
-        return substr($t, 3, 1) . substr($t, 2, 1) . substr($t, 1, 1) . substr($t, 0, 1);
-    }
-
-    public function uint64_t(int $v)
-    {
-        $t = hex2bin(str_pad(dechex($v), 16, "0", STR_PAD_LEFT));
-        return substr($t, 7, 1) . substr($t, 6, 1) . substr($t, 5, 1) . substr($t, 4, 1) . substr($t, 3, 1) . substr($t, 2, 1) . substr($t, 1, 1) . substr($t, 0, 1);
-    }
-
-    public function char(string $v, $l)
-    {
-        $sz = strlen($v);
-        if ($sz > $l - 1)
-            return false;
-        for ($i = 0; $i < $l - $sz; $i++)
-            $v .= hex2bin("00");
-        return $v;
-    }
-
-    public function construct($data)
+    private function strto_char(string $v)
     {
         $ret = "";
-        foreach($this->_fields as $field)
+		$l = strlen($v);
+		for ($i = 0; $i < $l;$i++)
         {
-            switch($field["type"])
-            {
-                case "uint8_t":
-                    $ret .= $this->uint8_t($data[$field["name"]]);
-                    break;
-                case "uint16_t":
-                    $ret .= $this->uint16_t($data[$field["name"]]);
-                    break;
-                case "uint32_t":
-                    $ret .= $this->uint32_t($data[$field["name"]]);
-                    break;
-                case "uint64_t":
-                    $ret .= $this->uint64_t($data[$field["name"]]);
-                    break;
-                case "char":
-                    $ret .= $this->char($data[$field["name"]],$field["length"]);
-                    break;
-            }
+            if (hexdec(bin2hex(substr($v, $i, 1))) == 0)
+                break;
+            $ret .= substr($v, $i, 1);
         }
         return $ret;
     }
+
+    public function construct($data)
+	{
+		$ret = "";
+		foreach($this->_fields as $field)
+		{
+			switch($field["type"])
+			{
+				case "uint8_t":
+					$ret .= $this->uint8_t($data[$field["name"]]);
+					break;
+				case "uint16_t":
+					$ret .= $this->uint16_t($data[$field["name"]]);
+					break;
+				case "uint32_t":
+					$ret .= $this->uint32_t($data[$field["name"]]);
+					break;
+				case "uint64_t":
+					$ret .= $this->uint64_t($data[$field["name"]]);
+					break;
+				case "char":
+					$ret .= $this->char($data[$field["name"]],$field["length"]);
+					break;
+			}
+		}
+		return $ret;
+	}
+
+	public function parse($str)
+	{
+		echo "parse str " . bin2hex($str) . "\n";
+
+		foreach($this->_fields as $field)
+		{
+			switch ($field["type"])
+			{
+				case "uint8_t":
+
+					echo "parse uint8_t of " . bin2hex(substr($str, 0, 1)) . "\n";
+					$this->_data[$field["name"]] = $this->strto_uint8_t(substr($str, 0, 1));
+                    $str = substr($str, 1);
+					break;
+				case "uint16_t":
+					echo "parse uint16_t of " . bin2hex(substr($str, 0, 2)) . "\n";
+					$this->_data[$field["name"]] = $this->strto_uint16_t(substr($str, 0, 2));
+					$str = substr($str, 2);
+					break;
+                case "uint32_t":
+                    echo "parse uint32_t of " . bin2hex(substr($str, 0, 4)) . "\n";
+                    $this->_data[$field["name"]] = $this->strto_uint32_t(substr($str, 0, 4));
+                    $str = substr($str, 4);
+                    break;
+                case "uint64_t":
+                    echo "parse uint32_t of " . bin2hex(substr($str, 0, 8)) . "\n";
+                    $this->_data[$field["name"]] = $this->strto_uint64_t(substr($str, 0, 8));
+                    $str = substr($str, 4);
+                    break;
+                case "char":
+                    $this->_data[$field["name"]] = $this->strto_char(substr($str, 0, $field["length"]));
+                    $str = substr($str, $field["length"]);
+                    break;
+            }
+		}
+	}
+
+	public function __get($name)
+	{
+		if (isset($this->_data[$name]))
+		{
+			return $this->_data[$name];
+		}
+		return null;
+	}
 }
 ?>
